fbc6feb6ac0e134effd1b1047c9c2fd70c5a0c92
Muyun2023 <ji.mu@northeastern.edu>
2025-04-08 16:26:01 -0700

    update

--- MetExplorer/DepartmentListView.swift
+++ MetExplorer/DepartmentListView.swift
@@ -26,6 +26,28 @@ class DepartmentViewModel {
     }
 }
 
+// Dictionary mapping departmentId to asset image names
+let departmentIcons: [Int: String] = [
+    1: "American Decorative Art",
+    //2: "Ancient Near Eastern Art", //?
+    3: "Ancient Near Eastern Art",
+    4: "Arms and Armor",
+    5: "Arts of Africa",
+    6: "Asian Art",
+    7: "The Cloisters",
+    8: "The Costume Institute",
+    9: "Drawing and Prints",
+    10: "Egyptian Art",
+    11: "European Paintings",
+    12: "European Sculpture",
+    13: "Greek and Roman Art"
+]
+
+// Return the correct icon name for a given department
+func iconName(for department: Department) -> String {
+    return departmentIcons[department.departmentId] ?? "The metropolitan Museum of Art"
+}
+
 // Main view displaying the list of departments
 struct DepartmentListView: View {
     @State private var viewModel = DepartmentViewModel()
@@ -33,9 +55,9 @@ struct DepartmentListView: View {
         NavigationView {
             List(viewModel.departments) { department in
                 HStack {
-                    Image(systemName: "paintpalette") //Icon
+                    //Image(systemName: "paintpalette") //Icon
+                    Image(iconName(for:department))
                         .resizable()
-                        .scaledToFit()
                         .frame(width: 40, height: 40)
                         .font(.title)
                         .padding()
@@ -75,3 +97,35 @@ struct DepartmentListView: View {
 #Preview {
     DepartmentListView()
 }
+
+
+/**func iconName(for department: Department) -> String {
+    switch department.departmentId {
+    case 1:
+        return "American Decorative Art"
+    case 2:
+        return "Ancient Near Eastern Art"
+    case 3:
+        return "Arms and Armor"
+    case 4:
+        return "Arts of Africa"
+    case 5:
+        return "Asian Art"
+    case 6:
+        return "The Cloisters"
+    case 7:
+        return "The Costume Institute"
+    case 8:
+        return "Drawing and Prints"
+    case 9:
+        return "Egyptian Art"
+    case 10:
+        return "European Paintings"
+    case 11:
+        return "European Sculpture"
+    case 12:
+        return "Greek and Roman Art"
+    default:
+        return "icon_default"
+    }
+}*/
